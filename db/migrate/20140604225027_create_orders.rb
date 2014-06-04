61ff08c723d6c7c2b15064a9bd5304ff2d238b0f
Frank C. Serravalli <Frankcserravalli@gmail.com>
2014-06-04 19:11:08 -0400

    Initial Commit of Rails 4 App

---
+++ db/migrate/20140604225027_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.integer :user_id, :volume, :user_stock_id, :volume_remaining
+      t.decimal :price, :value, :cost_basis, :capital_gain
+      t.string :type
+      t.timestamp :created_at, :updated_at
+    end
+    add_index :orders, :stock_id
+    add_index :orders, :user_id
+    add_index :orders, :type
+  end
+end
